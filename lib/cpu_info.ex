52230ab91e4e5673bdaca0377fc13578cbde0d21
Susumu Yamazaki <zacky1972@gmail.com>
2019-10-31 11:27:54 +0900

    implement the case of linux

--- lib/cpu_info.ex
+++ lib/cpu_info.ex
@@ -1,72 +1,151 @@
 defmodule CpuInfo do
-
   def os_type do
-  	case :os.type do
-  		{:unix, :linux} -> :linux
-  		{:unix, :darwin} -> :macos
-  		{:win32, _} -> :windows
-  		_ -> :other
-  	end
+    case :os.type() do
+      {:unix, :linux} -> :linux
+      {:unix, :darwin} -> :macos
+      {:win32, _} -> :windows
+      _ -> :other
+    end
   end
 
   def cpu_type do
-  	os_type() |> cpu_type_sub()
+    os_type() |> cpu_type_sub()
   end
 
-  defp cpu_type_sub :macos do
-  	if is_nil(System.find_executable("system_profiler")) do
-  		raise RuntimeError, message: "system_profiler isn't found."
-  	end
-
-  	case System.cmd("system_profiler", ["SPHardwareDataType"]) do
-  		{result, 0} -> result |> parse_macos
-  		{_, _} -> raise RuntimeError, message: "system_profiler don't work."
-  	end
+  def confirm_executable(command) do
+    if is_nil(System.find_executable(command)) do
+      raise RuntimeError, message: "#{command} isn't found."
+    end
   end
 
-  defp parse_macos message do
-  	trimmed_message = message |> split_trim
+  defp cpu_type_sub(:linux) do
+    confirm_executable("grep")
+    confirm_executable("sort")
+    confirm_executable("wc")
 
-  	cpu_type = Enum.filter(trimmed_message, & String.match?(&1, ~r/Processor Name/)) |> hd |> String.split() |> Enum.slice(2..-1) |> Enum.join(" ")
+    cpu_types =
+      :os.cmd('grep model.name /proc/cpuinfo | sort -u')
+      |> List.to_string()
+      |> String.split("\n")
+      |> Enum.map(&String.trim(&1))
+      |> Enum.reject(&(String.length(&1) == 0))
+      |> Enum.map(&String.split(&1))
+      |> Enum.map(&Enum.slice(&1, 3..-1))
+      |> Enum.map(&Enum.join(&1, " "))
 
-  	num_of_processors = Enum.filter(trimmed_message, & String.match?(&1, ~r/Number of Processors/)) |> hd |> match_to_integer()
+    cpu_type = hd(cpu_types)
 
-  	total_num_of_cores = Enum.filter(trimmed_message, & String.match?(&1, ~r/Total Number of Cores/)) |> hd |> match_to_integer()
+    num_of_processors =
+      :os.cmd('grep physical.id /proc/cpuinfo | sort -u | wc -l')
+      |> List.to_string()
+      |> String.trim()
+      |> String.to_integer()
 
-  	num_of_cores_of_a_processor = div(total_num_of_cores, num_of_processors)
+    num_of_cores_of_a_processor =
+      :os.cmd('grep cpu.cores /proc/cpuinfo | sort -u')
+      |> List.to_string()
+      |> String.trim()
+      |> match_to_integer()
 
-  	m_ht = Enum.filter(trimmed_message, & String.match?(&1, ~r/Hyper-Threading Technology/)) |> hd
+    total_num_of_cores = num_of_cores_of_a_processor * num_of_processors
 
-  	ht = if String.match?(m_ht, ~r/Enabled/) do
-  		:enabled
-  	else
-  		:disabled
-  	end
+    total_num_of_threads =
+      :os.cmd('grep processor /proc/cpuinfo | wc -l')
+      |> List.to_string()
+      |> String.trim()
+      |> String.to_integer()
 
-  	total_num_of_threads = total_num_of_cores * case ht do
-  		:enabled -> 2
-  		:disabled -> 1
-  	end
+    num_of_threads_of_a_processor = div(total_num_of_threads, num_of_processors)
 
-  	num_of_threads_of_a_processor = div(total_num_of_threads, num_of_processors)
+    ht =
+      if total_num_of_cores < total_num_of_threads do
+        :enabled
+      else
+        :disabled
+      end
 
-  	%{
-  		os_type: :macos,
-  		cpu_type: cpu_type,
-  		num_of_processors: num_of_processors,
-  		num_of_cores_of_a_processor: num_of_cores_of_a_processor, 
-  		total_num_of_cores: total_num_of_cores,
-  		num_of_threads_of_a_processor: num_of_threads_of_a_processor,
-  		total_num_of_threads: total_num_of_threads,
-  		hyper_threading: ht
-  	}
+    %{
+      os_type: :macos,
+      cpu_type: cpu_type,
+      cpu_types: cpu_types,
+      num_of_processors: num_of_processors,
+      num_of_cores_of_a_processor: num_of_cores_of_a_processor,
+      total_num_of_cores: total_num_of_cores,
+      num_of_threads_of_a_processor: num_of_threads_of_a_processor,
+      total_num_of_threads: total_num_of_threads,
+      hyper_threading: ht
+    }
+  end
+
+  defp cpu_type_sub(:macos) do
+    confirm_executable("system_profiler")
+
+    case System.cmd("system_profiler", ["SPHardwareDataType"]) do
+      {result, 0} -> result |> parse_macos
+      {_, _} -> raise RuntimeError, message: "system_profiler don't work."
+    end
+  end
+
+  defp parse_macos(message) do
+    trimmed_message = message |> split_trim
+
+    cpu_type =
+      Enum.filter(trimmed_message, &String.match?(&1, ~r/Processor Name/))
+      |> hd
+      |> String.split()
+      |> Enum.slice(2..-1)
+      |> Enum.join(" ")
+
+    cpu_types = [cpu_type]
+
+    num_of_processors =
+      Enum.filter(trimmed_message, &String.match?(&1, ~r/Number of Processors/))
+      |> hd
+      |> match_to_integer()
+
+    total_num_of_cores =
+      Enum.filter(trimmed_message, &String.match?(&1, ~r/Total Number of Cores/))
+      |> hd
+      |> match_to_integer()
+
+    num_of_cores_of_a_processor = div(total_num_of_cores, num_of_processors)
+
+    m_ht = Enum.filter(trimmed_message, &String.match?(&1, ~r/Hyper-Threading Technology/)) |> hd
+
+    ht =
+      if String.match?(m_ht, ~r/Enabled/) do
+        :enabled
+      else
+        :disabled
+      end
+
+    total_num_of_threads =
+      total_num_of_cores *
+        case ht do
+          :enabled -> 2
+          :disabled -> 1
+        end
+
+    num_of_threads_of_a_processor = div(total_num_of_threads, num_of_processors)
+
+    %{
+      os_type: :macos,
+      cpu_type: cpu_type,
+      cpu_types: cpu_types,
+      num_of_processors: num_of_processors,
+      num_of_cores_of_a_processor: num_of_cores_of_a_processor,
+      total_num_of_cores: total_num_of_cores,
+      num_of_threads_of_a_processor: num_of_threads_of_a_processor,
+      total_num_of_threads: total_num_of_threads,
+      hyper_threading: ht
+    }
   end
 
   defp split_trim(message) do
-  	message |> String.split("\n") |> Enum.map(& String.trim(&1))
+    message |> String.split("\n") |> Enum.map(&String.trim(&1))
   end
 
   defp match_to_integer(message) do
-  	Regex.run(~r/[0-9]+/, message) |> hd |> String.to_integer
+    Regex.run(~r/[0-9]+/, message) |> hd |> String.to_integer()
   end
 end
--- mix.exs
+++ mix.exs
@@ -20,7 +20,6 @@ defmodule CpuInfo.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-    ]
+    []
   end
 end
