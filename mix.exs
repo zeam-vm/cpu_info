e10351b65d3bfe96b2ddd93eac1fa4b2d66c4519
Susumu Yamazaki <zacky1972@gmail.com>
2019-10-31 14:19:20 +0900

    publish 0.0.1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule CpuInfo.MixProject do
       version: "0.0.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: [
+        api_reference: false,
+        main: "CpuInfo"
+      ]
     ]
   end
 
@@ -23,6 +29,21 @@ defmodule CpuInfo.MixProject do
     [
       {:earmark, "~> 1.4", only: :dev},
       {:ex_doc, "~> 0.21", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "CpuInfo: get CPU information, including a type, number of processors, number of physical cores and logical threads of a processor, and status of simultaneous multi-threads (hyper-threading)."
+  end
+
+  defp package() do
+    [
+      name: "cpu_info",
+      maintainers: [
+        "Susumu Yamazaki"
+      ],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/zeam-vm/cpu_info"}
     ]
   end
 end
